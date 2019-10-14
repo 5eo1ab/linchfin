e26c95dfd2815daeb67aac15fa2be0527e961972
shephexd <shephexd@gmail.com>
2019-10-14 23:35:54 +0900

    ADD utils for calculations

---
+++ linchfin/utils/calc.py
@@ -0,0 +1,24 @@
+import numpy as np
+import pandas as pd
+
+
+def calc_trading_value(prices, volumes) -> np.ndarray:
+    if not isinstance(prices, np.ndarray):
+        prices = np.array(prices)
+
+    if not isinstance(volumes, np.ndarray):
+        volumes = np.array(volumes)
+    return prices * volumes
+
+
+def get_dollar_bars(index, trading_values, threshold=2273626800):
+    def get_dollar_iter():
+        acc = 0
+        for i, _value in zip(index, trading_values):
+            acc += _value
+            if acc > threshold:
+                yield i, _value
+                acc = 0
+
+    df = pd.DataFrame(list(get_dollar_iter()), columns=['index', 'value'])
+    return df.set_index('index')
