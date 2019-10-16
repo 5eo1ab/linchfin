7e2f5e2530103095d46df732a47441f87bd862c7
Taekyung Han <shephexd@gmail.com>
2019-10-16 22:57:44 +0900

    ADD calc_total_return

--- linchfin/utils/calc.py
+++ linchfin/utils/calc.py
@@ -22,3 +22,7 @@ def get_dollar_bars(index, trading_values, threshold=2273626800):
 
     df = pd.DataFrame(list(get_dollar_iter()), columns=['index', 'dollar_value'])
     return df.set_index('index')
+
+
+def calc_total_return(prices):
+    return prices.adj_close.diff(1)[1:].sum()
