26ef355082ed20028fb189972f833fc273746b49
shephexd <shephexd@gmail.com>
2020-09-19 23:22:58 +0900

    #19 UPDATE Encoder IO Type

--- linchfin/base/encoder.py
+++ linchfin/base/encoder.py
@@ -5,12 +5,12 @@ from .dataclasses.value_types import Metric, Feature
 
 class Encoder:
     @classmethod
-    def encode(cls, data: Metric) -> Metric:
+    def encode(cls, data: Feature or Metric) -> Metric:
         raise NotImplementedError("Need to be implemented")
 
 
 class CorrelationEncoder(Encoder):
     @classmethod
-    def encode(cls, data: Metric) -> Metric:
+    def encode(cls, data: Feature) -> Metric:
         encoded_value = np.sqrt(np.divide((1 - data.value), 2))
         return Metric(name='dist', value=encoded_value)
