d33af6b65a22dd64b626022b381d69f30546a424
shephexd <shephexd@gmail.com>
2020-09-26 15:27:21 +0900

    #27 BUGFIX AssetName conflicted case

--- linchfin/base/dataclasses/entities.py
+++ linchfin/base/dataclasses/entities.py
@@ -67,18 +67,23 @@ class Cluster(Entity):
 @dataclass
 class Portfolio(Entity):
     portfolio_id: str = field(default_factory=uuid4)
-    weights: Dict[str, Weight] = field(default_factory=dict)
+    _weights: Dict[str, Weight] = field(default_factory=dict)
     asset_universe: AssetUniverse = field(default_factory=AssetUniverse)
 
-    def get_weights(self):
-        _weights = [(_asset.asset_name, self.weights[_asset.asset_id])
-                    for _asset in self.asset_universe.assets.values()]
-        return OrderedDict(_weights)
+    @property
+    def weights(self):
+        weights = OrderedDict()
+        for asset_id, _asset in self.asset_universe.assets.items():
+            print(asset_id)
+            if asset_id in self._weights:
+                raise KeyError(f"asset name is conflicted, {_asset}")
+            weights[_asset.asset_name] = self._weights[asset_id]
+        return weights
 
     def set_weights(self, weights: Dict[str, Weight] or pd.Series):
         for k, v in weights.items():
             _asset = self.asset_universe.assets[str(k)]
-            self.weights[_asset.asset_name] = Weight(v)
+            self._weights[_asset.asset_id] = Weight(v)
 
     def is_valid(self):
         if not round(sum(self.weights.values()), 4) == 1.0:
