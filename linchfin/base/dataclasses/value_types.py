51bae76aab64a29d2f5d79a0ef1f7f06f13d467b
shephexd <shephexd@gmail.com>
2020-09-26 15:16:01 +0900

    #25 UPDATE entities.py and value_types.py

--- linchfin/base/dataclasses/value_types.py
+++ linchfin/base/dataclasses/value_types.py
@@ -1,12 +1,21 @@
 from dataclasses import dataclass
 import numpy as np
 from decimal import Decimal
+import pandas as pd
 
 
 @dataclass
 class Feature:
     name: str
-    value: np.ndarray
+    value: pd.DataFrame
+
+    def __post_init__(self):
+        if isinstance(self.value, np.ndarray):
+            self.value = pd.DataFrame(self.value)
+
+    @property
+    def columns(self):
+        return self.value.columns
 
 
 @dataclass
@@ -16,4 +25,8 @@ class Metric:
 
 
 class Weight(Decimal):
+    pass
+
+
+class TimeSeries(pd.DataFrame):
     pass
