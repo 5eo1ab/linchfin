f04caff6f56d83efe7f86904ed0ed94f01372a83
shephexd <shephexd@gmail.com>
2020-10-15 22:38:14 +0900

    #55 UPDATE dollar_bar.py

--- linchfin/series/bars/dollar_bar.py
+++ linchfin/series/bars/dollar_bar.py
@@ -1,10 +1,17 @@
 import pandas as pd
 
 
-def get_dollar_bars(index, trading_values, threshold=2273626800):
+def find_loc(df, dates):
+    marks = []
+    for date in dates:
+        marks.append(df.index.get_loc(date))
+    return marks
+
+
+def get_dollar_bars(series: pd.Series, threshold=227362680000):
     def get_dollar_iter():
         acc = 0
-        for i, _value in zip(index, trading_values):
+        for i, _value in series.iteritems():
             acc += _value
             if acc > threshold:
                 yield i, _value
@@ -12,3 +19,15 @@ def get_dollar_bars(index, trading_values, threshold=2273626800):
 
     df = pd.DataFrame(list(get_dollar_iter()), columns=['index', 'dollar_value'])
     return df.set_index('index')
+
+
+if __name__ == '__main__':
+    from linchfin.data_handler.reader import DataReader
+
+    reader = DataReader()
+    ts = reader.get_trading_value(symbols=['AAPL'])
+    db = get_dollar_bars(series=ts['AAPL'])
+
+    from matplotlib import pyplot as plt
+    ts.plot(linestyle='-', markevery=find_loc(ts, db.index), marker='o', markerfacecolor='black')
+    plt.show()
