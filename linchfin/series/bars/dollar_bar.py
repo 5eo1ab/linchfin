8372e2664012adbbc54c2ee57718667357b7f116
shephexd <shephexd@gmail.com>
2020-10-17 23:21:15 +0900

    #55 UPDATE dollar_bar.py
    - ADD TimeSeriesHandler

--- linchfin/series/bars/dollar_bar.py
+++ linchfin/series/bars/dollar_bar.py
@@ -1,15 +1,30 @@
 import pandas as pd
+from linchfin.base.dataclasses.entities import Portfolio
+from linchfin.base.dataclasses.value_types import TimeSeries
 
 
-def find_loc(df, dates):
-    marks = []
-    for date in dates:
-        marks.append(df.index.get_loc(date))
-    return marks
+class TimeSeriesHandler:
+    def __init__(self, portfolio: Portfolio=None):
+        pass
 
+    def get_mean_trading_values(self, trading_values: TimeSeries, rule='W'):
+        portfolio_weekly_trading_mean_value = trading_values.resample(rule).sum().mean()
+        return portfolio_weekly_trading_mean_value
 
-def get_dollar_bars(series: pd.Series, threshold=227362680000):
-    def get_dollar_iter():
+    @staticmethod
+    def find_loc(df, dates):
+        marks = []
+        for date in dates:
+            marks.append(df.index.get_loc(date))
+        return marks
+
+    @classmethod
+    def get_dollar_bars(cls, series: pd.Series, threshold=227362680000):
+        df = pd.DataFrame(list(cls._get_dollar_iter(series=series, threshold=threshold)), columns=['index', 'dollar_value'])
+        return df.set_index('index')
+
+    @staticmethod
+    def _get_dollar_iter(series, threshold):
         acc = 0
         for i, _value in series.iteritems():
             acc += _value
@@ -17,17 +32,22 @@ def get_dollar_bars(series: pd.Series, threshold=227362680000):
                 yield i, _value
                 acc = 0
 
-    df = pd.DataFrame(list(get_dollar_iter()), columns=['index', 'dollar_value'])
-    return df.set_index('index')
-
 
 if __name__ == '__main__':
     from linchfin.data_handler.reader import DataReader
 
     reader = DataReader()
-    ts = reader.get_trading_value(symbols=['AAPL'])
-    db = get_dollar_bars(series=ts['AAPL'])
+    # symbol = 'AAPL'
+    symbols = ['AAPL', 'PIO']
+    closed_prices = reader.get_adj_close_price(symbols=symbols)
+    trading_values = reader.get_trading_value(symbols=symbols)
 
-    from matplotlib import pyplot as plt
-    ts.plot(linestyle='-', markevery=find_loc(ts, db.index), marker='o', markerfacecolor='black')
-    plt.show()
+    time_series_handler = TimeSeriesHandler()
+    _mean_trading_value = time_series_handler.get_mean_trading_values(trading_values=trading_values)
+
+    print(_mean_trading_value)
+    # db = time_series_handler.get_dollar_bars(series=ts[symbol], threshold=2273626800)
+    #
+    # from matplotlib import pyplot as plt
+    # closed_prices.plot(linestyle='-', markevery=time_series_handler.find_loc(ts, db.index), marker='o', markerfacecolor='black')
+    # plt.show()
