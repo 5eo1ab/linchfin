d9a91885a4dedc724e4e468045745a17282c9f98
shephexd <shephexd@gmail.com>
2021-02-11 23:44:37 +0900

    #48 WIP backtest.py
    ADD BackTestRunner

--- linchfin/simulation/backtest.py
+++ linchfin/simulation/backtest.py
@@ -3,42 +3,82 @@ from linchfin.base.dataclasses.value_types import TimeSeries, Weight
 from linchfin.common.calc import calc_portfolio_return, calc_daily_returns
 
 
+class BackTestConfig:
+    pass
+
+
+class BackTestRunner:
+    def __init__(self, portfolio: Portfolio, config: BackTestConfig):
+        self.step = 0
+        self.prev_value = None
+        self._value = None
+        self.model_portfolio = Portfolio(weights=portfolio.weights)
+        self.current_portfolio = Portfolio(weights=portfolio.weights)
+        self.acc_trading_values = 0
+        self.config = config
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        self.calc_current_portfolio()
+        self.acc_trading_values += self.calc_trading_values()
+        return self.step, self.value
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, value):
+        self.prev_value = self._value
+        self._value = value
+
+    def calc_trading_values(self):
+        return (self.current_portfolio.to_series() * self.value.loc['Adj Close'] * self.value.loc['Volume']).sum()
+
+    def calc_current_portfolio(self):
+        if self.prev_value is not None:
+            changes = (1 + (self.value.loc['Adj Close'] - self.prev_value.loc['Adj Close']) / self.value.loc['Adj Close'])
+            new_weights = self.current_portfolio.to_series() * changes
+            new_weights = new_weights / new_weights.sum()
+            self.current_portfolio.set_weights(new_weights)
+            print(self.current_portfolio.to_series())
+            return
+        return
+
+    def check_rebalancing_condition(self):
+        pass
+
+
 class BackTestSimulator:
     def run(self, portfolio: Portfolio, daily_returns: TimeSeries):
         portfolio_yield = calc_portfolio_return(portfolio=portfolio, daily_returns=daily_returns)
         return portfolio_yield
 
+    def iter_runner(self, runner: BackTestRunner, time_series: TimeSeries):
+        for i, row in time_series.iterrows():
+            runner.step = i
+            runner.value = row
+            k = next(runner)
+            print(k)
+
 
 if __name__ == '__main__':
     from linchfin.data_handler.reader import DataReader
-    from linchfin.data_handler.wrangler import DataWrangler
-
     weights = {
-        'JDST': Weight('0.13283863778299764835111318461713381111621856689453125'),
-        'DUST': Weight('0.13283863778299764835111318461713381111621856689453125'),
-        'JNUG': Weight('0.133438073337116713812378065995289944112300872802734375'),
-        'NUGT': Weight('0.0701666866303975178542629009825759567320346832275390625'),
-        'GDXJ': Weight('0.0701666866303975178542629009825759567320346832275390625'),
-        'SGDM': Weight('0.035410889212107880819058181032232823781669139862060546875'),
-        'RING': Weight('0.035410889212107880819058181032232823781669139862060546875'),
-        'GDX': Weight('0.04324006025578529255337656422852887772023677825927734375'),
-        'TAN': Weight('0.02571939632182819457373312843628809787333011627197265625'),
-        'SKYY': Weight('0.02571939632182819457373312843628809787333011627197265625'),
-        'OIH': Weight('0.038697493599623655757824280954082496464252471923828125'),
-        'IGF': Weight('0.038697493599623655757824280954082496464252471923828125'),
-        'ACWV': Weight('0.0383184102632821155243192379202810116112232208251953125'),
-        'SMH': Weight('0.0197456481130713346061611446202732622623443603515625'),
-        'IXN': Weight('0.0197456481130713346061611446202732622623443603515625'),
-        'URTH': Weight('0.034537437896782270663198488591660861857235431671142578125'),
-        'VT': Weight('0.034537437896782270663198488591660861857235431671142578125'),
-        'ACWI': Weight('0.035140928888262860929625475137072498910129070281982421875'),
-        'IXC': Weight('0.0178150740709680059647990191251665237359702587127685546875'),
-        'GUNR': Weight('0.0178150740709680059647990191251665237359702587127685546875')}
+        'SKYY': Weight('0.4'),
+        'OIH': Weight('0.3'),
+        'GUNR': Weight('0.3')}
+
     _port = Portfolio(weights=weights)
     backtester = BackTestSimulator()
 
     data_reader = DataReader(start='2019/01/01', end='2020/09/01')
-    data_wrangler = DataWrangler()
-    ts = data_reader.get_adj_close_price(symbols=list(_port.weights.keys()))
-    daily_returns = calc_daily_returns(time_series=ts)
-    backtester.run(portfolio=_port, daily_returns=daily_returns)
+    ts = data_reader.get_price(symbols=list(_port.weights.keys()))
+    # ts = data_reader.get_adj_close_price(symbols=list(_port.weights.keys()))
+    # daily_returns = calc_daily_returns(time_series=ts)
+    # backtester.run(portfolio=_port, daily_returns=daily_returns)
+
+    backtest_runner = BackTestRunner(portfolio=_port)
+    backtester.iter_runner(runner=backtest_runner, time_series=ts)
