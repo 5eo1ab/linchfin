eb47954108ab5e46ebc162b5064efdc0456c16d2
shephexd <shephexd@gmail.com>
2021-04-28 23:38:20 +0900

    #97 UPDATE dataclasses I/F

--- linchfin/base/encoder.py
+++ linchfin/base/encoder.py
@@ -1,6 +1,10 @@
+from typing import Union
 import numpy as np
-
+import pandas as pd
 from .dataclasses.value_types import Metric, Feature
+
+
+CorrValueType = Union[Feature, pd.DataFrame]
 
 
 class Encoder:
@@ -11,6 +15,8 @@ class Encoder:
 
 class CorrelationEncoder(Encoder):
     @classmethod
-    def encode(cls, corr: Feature) -> Metric:
+    def encode(cls, corr: CorrValueType) -> Metric:
+        if isinstance(corr, pd.DataFrame):
+            corr = Feature(name='corr', value=corr)
         encoded_value = np.sqrt(np.divide((1 - corr.value), 2))
         return Metric(name='dist', value=encoded_value)
